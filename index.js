389e4ac90682dcd5d8592f76197d14df692a0d9b
hariprostack123 <128031941+hariprostack123@users.noreply.github.com>
2023-11-22 16:02:21 +0530

    js11

---
+++ index.js
@@ -0,0 +1,16 @@
+  // useful string proprties & methods
+
+  let userName = " Pro stack ";
+  let phoneNumber = "123-456-7890";
+
+ // console.log(userName.length);
+ // console.log(userName.charAt(0));
+ // console.log(userName.indexOf("o"));
+ // console.log(userName.indexOf("o"));
+ // userName = userName.trim();
+ // userName = userName.toUpperCase();
+ //userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-","");
+
+ console.log(phoneNumber);
